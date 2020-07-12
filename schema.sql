4d0e54c4a013be7dbe5a53b322d6c500645e9af8
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-12 12:52:22 -0500

    added routes and mysql connection to server js

--- schema.sql
+++ schema.sql
@@ -3,6 +3,8 @@ DROP DATABASE IF EXISTS FriendFinder_DB;
 
 CREATE DATABASE FriendFinder_DB;
 
+USE FriendFinder_DB;
+
 DROP TABLE IF EXISTS friendFinder;
 
 CREATE TABLE friendFinder
@@ -10,6 +12,7 @@ CREATE TABLE friendFinder
     id INT AUTO_INCREMENT NOT NULL,
         PRIMARY KEY(id),
     person_name VARCHAR(255) NOT NULL,
+    image_link VARCHAR(4000) NOT NULL,
     question1 INT NOT NULL, -- Your mind is always buzzing with unexplored ideas and plans.
     question2 INT NOT NULL, -- Generally speaking, you rely more on your experience than your imagination.
     question3 INT NOT NULL, -- You find it easy to stay relaxed and focused even when there is some pressure.
--- server.js
+++ server.js
@@ -3,9 +3,45 @@ var express = require("express");
 var mysql = require("mysql");
 var app = express();
 
+var PORT = process.env.PORT || 8001;
 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "FriendFinder_DB"
+});
 
+connection.connect(function(err){
+    if(err){
+        console.error(`Connection Error: ${err.stack}`);
+        return;
+    }
+    console.log(`Successful connection. Connection Id: ${connection.threadId}`);
+})
 
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+app.get("/", function(req,res) {
+    res.sendFile(path.join(__dirname,"index.html"));
+})
+
+app.post("/addFriend", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
+
+    res.send(newFriend);
+})
+
+app.listen(PORT, function() {
+    
+    console.log(`Server Listening on: http://localhost:${PORT}`);
+    
+})
 
 
 
